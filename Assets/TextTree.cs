541b792ad6a429bd8e5bf7fb0f2560545494ee22
lambmeow <javiers2001@live.com>
2018-01-19 21:38:42 -0600

    FirstCommit

---
+++ Assets/TextTree.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class TextTree : MonoBehaviour {
+	public TextThing[] text;
+	    
+}
+[System.Serializable]
+public class TextThing{
+	public string text;
+
+	public UnityEvent e;
+
+	
+	
+}
